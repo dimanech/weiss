12512e047212055389359bae7b16fa58a40ab56d
dimaNech <dimanechepurenko@gmail.com>
2012-03-29 19:49:21 +0300

    fixed stylized form bug. some improviments

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $('#nav li a').click(function(){
        $('#nav li a').removeClass('active'); 
-       $(this).addClass('active'); 
+       $(this).addClass('active');
+       return false;
     });
     
     $('#twocolumn-s').click(function() {
@@ -12,6 +13,21 @@ $(document).ready(function() {
     });
     $('#threecolumn').click(function() {
         $('body').removeClass().addClass('two-sidebars');
+    });
+    
+    // tabs
+    
+    $('.tabcontent').hide();
+    $('.tabcontent:first').show();
+    $('.tab-nav:first').addClass('selected');
+    $('.tab-nav a').click(function () {
+        $('.tab-nav a').removeClass('selected');
+        $(this).addClass('selected');
+
+        var current = $(this).attr('href');
+        $('.tabcontent:visible').animate({height:'hide'});
+        $(current).animate({height:'show'},'slow');
+        return false;
     });
 
 });
