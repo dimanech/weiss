27f391394ea750f52db61699d174ea2bd4b2b4f4
dimaNech <dimanechepurenko@gmail.com>
2012-04-29 13:49:12 +0300

    accordion moved in scripts.js, layout scripts moved in check.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,72 @@
 $(document).ready(function() {
-    $('#nav li a').click(function(){
-       $('#nav li a').removeClass('active'); 
-       $(this).addClass('active');
-       return false;
-    });
-    
-    $('#twocolumn-s').click(function() {
-        $('body').removeClass().addClass('sidebar-second');
-    });
-    $('#twocolumn-f').click(function() {
-        $('body').removeClass().addClass('sidebar-first');
-    });
-    $('#threecolumn').click(function() {
-        $('body').removeClass().addClass('two-sidebars');
-    });
-    
+
     // tabs
-    
+
+    contentTabs();
+
+    // accordion
+
+    accordion();
+
+});
+
+function contentTabs() {
     $('.tabcontent').hide();
     $('.tabcontent:first').show();
-    $('.tab-nav:first').addClass('selected');
-    $('.tab-nav a').click(function () {
-        $('.tab-nav a').removeClass('selected');
-        $(this).addClass('selected');
+    $('.tab-nav a:first').addClass('selected');
 
-        var current = $(this).attr('href');
-        $('.tabcontent:visible').animate({height:'hide'});
-        $(current).animate({height:'show'},'slow');
+    $('.tab-nav a').click(function() {
+        if($(this).hasClass('selected')) {
+            return false;
+        } else {
+            $('.tab-nav a').removeClass('selected');
+            $(this).addClass('selected');
+
+            $('.tabcontent').filter(":visible").fadeOut(200);
+            $($(this).attr('href')).fadeIn(200);
+
+            return false;
+        }
+    });
+}
+
+function accordion() {
+    var links = $('.accordion > section > header > h3 > a'), descriptions = $('.accordion .accordion-content');
+
+    // init
+    links.removeClass('active');
+    descriptions.hide();
+    $(links[0]).addClass('active');
+    $(descriptions[0]).show();
+
+    $('.accordion .expand-all').live('click', function() {
+        links.addClass('active');
+        descriptions.slideDown();
         return false;
     });
 
-});
+    $('.accordion .collapse-all').live('click', function() {
+        links.removeClass('active');
+        descriptions.slideUp();
+        return false;
+    });
+
+    $('.accordion .collapse').live('click', function() {
+        $(this).closest('section').find('header > h3 > a').removeClass('active');
+        $(this).closest('.accordion-content').slideUp();
+        return false;
+    });
+
+    links.click(function() {
+        var self = $(this);
+        if(self.hasClass('active')) {
+            self.removeClass('active').parent().parent().next('.accordion-content').slideUp();
+        } else {
+            links.removeClass('active');
+            self.addClass('active');
+            descriptions.slideUp();
+            self.parent().parent().next().slideDown();
+        }
+        return false;
+    });
+}
