d17c99a7bdd237027ae2b2e8cb57b11edc05b8d9
dimaNech <dimanechepurenko@gmail.com>
2012-09-28 15:59:34 +0300

    form exmaple styles added

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,30 +1,58 @@
 $(document).ready(function () {
 
-    contentTabs();
+	contentTabsUnordered();
+	//contentTabsOrdered();
 
 });
 
-function contentTabs() {
-    var links = $('.js-tab-ln'),
-        content = $('.js-tab-content'),
-        activeClass = 'js-tab-ln_selected';
+function contentTabsUnordered() {
+	var links = $('.js-tab-ln'),
+		content = $('.js-tab-content'),
+		activeClass = 'js-tab-ln_selected';
 
-    content.hide();
-    $(content[0]).show();
-    $(links[0]).addClass(activeClass);
+	content.hide();
+	$(content[0]).show();
+	$(links[0]).addClass(activeClass);
 
-    links.click(function () {
-        if ($(this).hasClass(activeClass)) {
-            return false;
-        } else {
-            var self = $(this);
-            links.removeClass(activeClass);
-            self.addClass(activeClass);
+	links.click(function() {
+		var self = $(this);
 
-            content.filter(":visible").hide();
-            $(self.attr('href')).fadeIn(200);
-            $('body').animate({scrollTop:self.offset().top}, 500);
-            return false;
-        }
-    });
+		if (self.hasClass(activeClass)) {
+			return false;
+		} else {
+			links.removeClass(activeClass);
+			self.addClass(activeClass);
+
+			content.hide();
+			$(self.attr('href')).fadeIn(200);
+			$('body').animate({scrollTop:self.offset().top}, 500);
+			return false;
+		}
+	});
 }
+
+/*
+function contentTabsOrdered() {
+	var links = $('.js-tab-ln'),
+		content = $('.js-tab-content'),
+		activeClass = 'js-tab-ln_selected';
+
+	content.hide();
+	$(content[0]).show();
+	$(links[0]).addClass(activeClass);
+
+	links.click(function() {
+		var self = $(this);
+
+		if (self.hasClass(activeClass)) {
+			return false;
+		} else {
+			links.removeClass(activeClass);
+			self.addClass(activeClass);
+
+			content.hide();
+			content.eq(self.index(links)).fadeIn(200);
+			return false;
+		}
+	});
+}*/
