2a223e2537dc90b4ef958e714bafe7ef31b155c9
dimaNech <dimanechepurenko@gmail.com>
2012-12-15 00:11:52 +0200

    FIXED: form impro

--- js/main.js
+++ js/main.js
@@ -6,6 +6,46 @@ $(document).ready(function() {
     //$(".js-no-native input:check, .no-native input:radio").checkbox();
 
     //$('input[placeholder], textarea[placeholder]').placeholder();
+
+
+	var sertBlock = $('.icon-list-block').find('a');
+
+	sertBlock.live('mouseenter', function() {
+		var self = $(this);
+
+		setSertificats({
+			'tooltip'  : self.find('.tooltip'),
+			'sert_ico' : self.find('.sert-ico')
+		}, 'enter');
+	});
+
+	sertBlock.live('mouseleave', function() {
+		var self = $(this);
+
+		setSertificats({
+			'tooltip'  : self.find('.tooltip'),
+			'sert_ico' : self.find('.sert-ico')
+		}, 'leave');
+
+	});
+
 });
 
+function setSertificats(nodes, type) {
 
+	var params = {
+		enter : {
+			tooltip: { opacity: 1, top: '-20px' },
+			sert_ico: { bottom: '-15px' }
+		},
+		leave : {
+			tooltip: { opacity: 0, top: '-10px' },
+			sert_ico: { bottom: 0 }
+		},
+		time: 300
+	}
+
+	for( var i in nodes ) {
+		nodes[i].animate( params[type][i], params.time, 'linear');
+	}
+}
