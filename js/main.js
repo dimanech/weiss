fa0bc0b8e851044cd4e520d824e94b814345c00c
dimaNech <dimanechepurenko@gmail.com>
2013-02-10 23:41:50 +0200

    CHANGED: jquery, htmlshiv versions. ADDED: jstyling input styling plugin.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $.foundation.customForms.appendCustomMarkup();
+	$.jStyling.createSelect($('.form_custom select'));
+	$.jStyling.createCheckbox($('.form_custom input[type=checkbox]'));
+	$.jStyling.createRadio($('.form_custom input[type=radio]'));
+	$.jStyling.createFileInput($('.form_custom input[type=file]'));
+	$.jStyling({'fileButtonText': 'Browse file'})
 
-    //$(".js-no-native select").sb();
-    //$(".js-no-native input:check, .no-native input:radio").checkbox();
+	//$('input[placeholder], textarea[placeholder]').placeholder();
 
-    //$('input[placeholder], textarea[placeholder]').placeholder();
-
+	//$.foundation.customForms.appendCustomMarkup();
+	//$(".js-no-native select").sb();
+	//$(".js-no-native input:check, .no-native input:radio").checkbox();
 });
