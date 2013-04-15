18c4d95d4de687af5843f5f28f1a061412f6590a
dimaNech <dimanechepurenko@gmail.com>
2013-04-15 20:46:05 +0300

    CHANGED: sass file structure for better work with WebStorm. Minor changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-$(document).ready(function () {
+;
+var scripts = scripts || {};
 
-	$.jStyling.createSelect($('.form_custom select'));
-	$.jStyling.createCheckbox($('.form_custom input[type=checkbox]'));
-	$.jStyling.createRadio($('.form_custom input[type=radio]'));
-	$.jStyling.createFileInput($('.form_custom input[type=file]'));
-	$.jStyling({'fileButtonText': 'Browse file'})
+scripts.Common = {
+	detecting: function () {
+		// global technology detecting
+		$('html').removeClass('no-js');
+	},
 
-	//$('input[placeholder], textarea[placeholder]').placeholder();
+	jStylingInit: function () {
+		if ($('.form_custom').length) {
+			$.jStyling.createSelect($('.form_custom select'));
+			$.jStyling.createCheckbox($('.form_custom input[type=checkbox]'));
+			$.jStyling.createRadio($('.form_custom input[type=radio]'));
+			$.jStyling.createFileInput($('.form_custom input[type=file]'));
+			$.jStyling({'fileButtonText': 'Browse file'})
+		}
+	},
 
-	//$.foundation.customForms.appendCustomMarkup();
-	//$(".js-no-native select").sb();
-	//$(".js-no-native input:check, .no-native input:radio").checkbox();
-});
+	jsPlaceholderInit: function () {
+		$('input[placeholder], textarea[placeholder]').placeholder();
+	},
+
+	init: function () {
+		var scripts = this;
+
+		scripts.detecting();
+
+		$(function () {
+//			if (!window.Modernizr || !Modernizr.generatedcontent) {
+//
+//			}
+
+			scripts.jStylingInit();
+		});
+
+		return scripts;
+	}
+};
+
+scripts.Common.init();
